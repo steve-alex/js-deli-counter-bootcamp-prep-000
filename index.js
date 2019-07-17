9df02b6f3a52b737cf776b15206a9a01157bc9a2
effective-role-2041 <steve.alex@hotmail.co.uk>
2019-07-17 14:10:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-katzDeliLine
+var counter = 0;
 
-function takeANumber(katzDeliLine, name){
-  katzDeliLine.push(name);
-  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+function takeANumber(katzDeliLine){
+  katzDeliLine.push(++counter);
+  return `Welcome you are number ${counter}`
 }
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length === 0){
     return "There is nobody waiting to be served!"
   } else {
-    return `Currently serving ${katzDeliLine.shift()}.`
+    return `Currently serving ${katzDeliLine.shift()}.`;
   }
 }
 
@@ -17,10 +17,11 @@ function currentLine(katzDeliLine){
   if (katzDeliLine.length === 0){
     return "The line is currently empty."
   } else {
-    var line = ''
+    var line = "The line is currently: "
     for (var i=0; i<(katzDeliLine.length - 1); i++){
       line += `${i+1}. ${katzDeliLine[i]}, `;
-    }
+    } 
     line += `${katzDeliLine.length}. ${katzDeliLine[(katzDeliLine.length) - 1]}`
-  } return `The line is currently: ${line}`
+    return line
+  }
 }
